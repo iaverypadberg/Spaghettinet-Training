83a9469a5d8232b7d7a64b37af2f3894ae5ce82c
deepfish <isaac.padberg@deepanalyticsllc.com>
2022-04-07 13:14:44 -0400

    Update class names

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -26,8 +26,10 @@ FLAGS = flags.FLAGS
 
 
 def class_text_to_int(row_label):
-    if row_label == '9v':
+    if row_label == 'class-name-here':
         return 1
+    elif row_label == "class-name-here":
+        return 2
     else:
         return None
 
